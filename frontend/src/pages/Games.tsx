1709fbedf931c2535f411aec090f3483c9f46739
keysax0 <elhajj.angelo9@gmail.com>
2025-11-11 14:22:13 -0500

    TailWind setup

---
+++ frontend/src/pages/Games.tsx
@@ -0,0 +1,9 @@
+function Games() {
+    return (
+        <div className="p-8 text-center">
+            <h2 className="text-3xl font-bold mb-4"></h2>
+            <p className="text-gray-700"></p>
+        </div>
+    );
+}
+export default Games;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div className="p-8 text-center">
+            <h2 className="text-4xl text-blue-600 font-bold mb-4"></h2>
+            <p className="text-gray-700"></p>
+        </div>
+    );
+}
+export default Home;
